8cf0a729676fd526db73f1004bfe3313171854f9
Jake Larson <jake.j.larson@gmail.com>
2025-05-06 01:03:52 -0500

    Add C++ test executable and update CMake configuration

---
+++ tests/source/mvn_ds_cpp_test.cpp
@@ -0,0 +1,24 @@
+// filepath: tests/source/mvn_ds_cpp_test.cpp
+/*
+ * Copyright (c) 2024 Jake Larson
+ */
+#include <cstdlib> // For EXIT_SUCCESS, EXIT_FAILURE
+#include <iostream>
+#include <mvn_ds/mvn_ds_string.h> // Include one of the library headers
+
+int main()
+{
+    mvn_string_t *test_string = mvn_string_new("Hello from C++"); // Call a C function
+
+    if (test_string == NULL) {
+        std::cerr << "Failed to create mvn_string from C++" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Successfully created mvn_string: " << test_string->data
+              << std::endl; // Changed cstr to data
+
+    mvn_string_free(test_string); // Call another C function
+
+    return EXIT_SUCCESS;
+}
