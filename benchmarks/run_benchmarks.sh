7fc7bff2f77b5d032598e65825023c367bfa7660
Jake Larson <jake.j.larson@gmail.com>
2025-05-07 21:40:02 -0500

    Reduce benchmark time limits for improved performance tracking

--- benchmarks/run_benchmarks.sh
+++ benchmarks/run_benchmarks.sh
@@ -4,10 +4,10 @@ set -e
 
 # Define time limits for each benchmark (in seconds)
 declare -A BENCHMARK_LIMITS=(
-    ["mvn_ds_arr_benchmark"]=10
-    ["mvn_ds_hmap_benchmark"]=15
-    ["mvn_ds_string_benchmark"]=8
-    ["mvn_ds_primitives_benchmark"]=12
+    ["mvn_ds_arr_benchmark"]=5
+    ["mvn_ds_hmap_benchmark"]=5
+    ["mvn_ds_string_benchmark"]=5
+    ["mvn_ds_primitives_benchmark"]=5
 )
 
 # Directory containing the benchmark executables
